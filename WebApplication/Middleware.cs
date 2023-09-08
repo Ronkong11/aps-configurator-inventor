7240b1ca33a4e5c9b8022b93ff8f1e03de55d2ac
Ronald King <ronking@screwconveyors.com>
2023-09-07 21:42:19 -0500

    updated

---
+++ WebApplication/Middleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace WebApplication
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class MiddlewareBase1(RequestDelegate next)
+    {
+        private readonly RequestDelegate _next = next;
+
+        public Task Invoke(HttpContext httpContext)
+        {
+
+            return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<MiddlewareBase1>();
+        }
+    }
+}
